3f789bfaae51d4a9072eaea52bcb7e63f8234f1a
Sulav Bajracharya <sulavb22@gmail.com>
2026-03-04 10:57:33 +0545

    initialization of the project

---
+++ backend/rector.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/bootstrap',
+        __DIR__.'/database',
+        __DIR__.'/public',
+    ])
+    ->withSets([
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+    ]);
